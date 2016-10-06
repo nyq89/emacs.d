a2ce1c3c51f78a935d738d4f346e13238eba9b5c
Robert Nyquist <robert@footballaddicts.com>
2016-10-06 18:33:40 +0200

    Just joined a cult

---
+++ init.el
@@ -0,0 +1,23 @@
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package doom-theme
+  :config (load-theme 'doom-one t))
+
+(use-package evil
+  :ensure t
+  ;:init (setq evil-want-C-u-scroll t)
+  :config (progn
+	    (evil-mode 1)
+            (defalias #'forward-evil-word #'forward-evil-symbol)))
+(custom-set-variables
+ '(package-selected-packages (quote (evil use-package doom-themes))))
+(custom-set-faces
+ )
